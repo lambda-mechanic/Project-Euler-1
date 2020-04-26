9ddeb82b7c4605a63f89ca1db114774f0241073b
Liam C <44451198+liamchiasson@users.noreply.github.com>
2020-04-26 19:21:05 -0400

    Added files for project euler problem 1
    Included cpp and txt versions

---
+++ eulerP1.cpp
@@ -0,0 +1,20 @@
+// eulerP1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	int sum = 0;
+	for (int i = 0; i < 1000; i++) {
+		if ( ((i % 3) == 0) || ((i % 5) == 0) ) {
+			sum += i;
+		}
+	}
+	std::cout << sum << std::endl;
+
+
+    return 0;
+}
+
